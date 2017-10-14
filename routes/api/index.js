754145abc8cb48cc4411cae98205bb83da5c2375
benjhill78 <benjhill78@gmail.com>
2017-10-14 08:10:11 -0400

    api setup

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.json({ text: "Musiclist API Text"});
-});
-
 
 
 module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require("../../models/user");
+
+router.get("/List", (req, res, next) => {
+    User.find((err, users) => {
+        if(err) {
+            res.send(err);
+        }
+        res.json(users);
+        
+    });
+});
+
+
+module.exports = router;
